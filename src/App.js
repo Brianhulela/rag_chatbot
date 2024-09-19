8ecf360125f2733296b0e7fbbf289f63befc6d70
Brianhulela <101281239+Brianhulela@users.noreply.github.com>
2024-09-19 18:58:40 +0200

    Added template code for demo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
+
+  const [count, setCount] = React.useState(0);
+  const [negativeCount, setNegativeCount] = React.useState(0); 
+
+  const addCount = () => {
+    setCount(count + 1);
+  }
+
+  React.useEffect(() => {
+    setNegativeCount(-count);
+  }, [count]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>React App</h1>
+      <p>This is a React app created using Vite and ESBuild.</p>
+      <input type="text" placeholder="Enter your prompt" />
+      <button>negative: {negativeCount}</button>
+      <button onClick={addCount}>counter: {count}</button>
     </div>
   );
 }
