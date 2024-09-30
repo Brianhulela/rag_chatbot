bfacf692304cb70f0dc955b3a854cc90f1d1fd2a
Brianhulela <101281239+Brianhulela@users.noreply.github.com>
2024-09-30 21:52:13 +0200

    Installed MUI

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
 import React from "react";
+import SignIn from "./pages/SignIn";
 
 function App() {
 
-  const [count, setCount] = React.useState(0);
-  const [negativeCount, setNegativeCount] = React.useState(0); 
-
-  const addCount = () => {
-    setCount(count + 1);
-  }
-
-  React.useEffect(() => {
-    setNegativeCount(-count);
-  }, [count]);
-
-  let name = "John";
 
   return (
     <div>
-      <h1>React App</h1>
-      <p>This is a React app created using Vite and ESBuild.</p>
-      <input type="text" placeholder="Enter your prompt" />
-      <button>negative: {negativeCount}</button>
-      <button onClick={addCount}>counter: {count}</button>
+      <SignIn/>
     </div>
   );
 }
