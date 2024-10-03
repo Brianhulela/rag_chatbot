40dd765b5c674d5fbe82ced95867499946a3151b
Brianhulela <101281239+Brianhulela@users.noreply.github.com>
2024-10-03 22:45:55 +0200

    Added createdAt and updatedAt attributes to chat

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -2,6 +2,7 @@ import React from "react";
 import { addChat } from "../firebase/Database";
 import { Button, Container } from "@mui/material";
 import useAuth from "../firebase/useAuth";
+import { Timestamp } from "firebase/firestore";
 
 function Chat() {
   const { user } = useAuth();
@@ -11,11 +12,13 @@ function Chat() {
       const testChat = {
         title: "Test Chat",
         uid: user.uid,
+        createdAt: Timestamp.now(),
+        updatedAt: Timestamp.now(),
       };
       await addChat(testChat);
     }
   };
-  
+
   return (
     <Container>
       <Button onClick={handleAddChat} variant="contained">
