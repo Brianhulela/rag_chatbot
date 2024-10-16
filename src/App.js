d9a23f5f6131d5bf00fb32414835e2385d84547b
Brianhulela <101281239+Brianhulela@users.noreply.github.com>
2024-10-16 21:31:44 +0200

    Added Routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Chat from "./pages/Chat";
@@ -6,9 +7,17 @@ import Home from "./pages/Home";
 
 function App() {
   return (
-    <div>
-      <Home/>
-    </div>
+    <Router>
+      <div>
+        {/* Define the routes for different pages */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/chat" element={<Chat />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
